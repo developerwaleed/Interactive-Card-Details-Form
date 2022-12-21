5b529b2733ffce5ce7792f486436421ed7c45ca7
Waleed Amjad <waleedamjad99@hotmail.com>
2022-12-22 03:58:01 +0500

    Create Home Page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,21 @@
 import React from 'react'
+import './Home.css';
 import CardFrontView from '../CardFrontView/CardFrontView'
 import CardBackView from '../CardBackView/CardBackView'
 import Form from '../Form/Form'
 
 function Home() {
   return (
-    <>
-        {/* <CardFrontView /> */}
-        {/* <CardBackView /> */}
+    <div className='home'>
+        <div className='CardBackview-home-div'>
+          <CardBackView />
+        </div>
+        <div className='CardFrontview-home-div'>
+        <CardFrontView />
+        </div>
+        <div className='formdiv'></div>
         <Form />
-    </>
+    </div>
   )
 }
 

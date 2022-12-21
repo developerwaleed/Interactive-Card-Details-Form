c09425367ead1416fe621e2e7264ae870112c68c
Waleed Amjad <waleedamjad99@hotmail.com>
2022-12-21 21:58:17 +0500

    Create Back View for Card

---
+++ src/components/CardBackView/CardBackView.js
@@ -0,0 +1,14 @@
+    import React from 'react'
+    import './CardBackView.css'
+
+    function CardBackView() {
+    return (
+        <div className='CardBackView'>
+            <div className='cvc-number'>
+                123
+            </div>
+        </div>
+    )
+    }
+
+    export default CardBackView

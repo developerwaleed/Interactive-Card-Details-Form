736d1d7e614be18e6d945ef83497ecdcf0094a1b
Waleed Amjad <waleedamjad99@hotmail.com>
2022-12-22 01:04:29 +0500

    Preapre files for mobile design

---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import CardFrontView from '../CardFrontView/CardFrontView'
+import CardBackView from '../CardBackView/CardBackView'
+import Form from '../Form/Form'
+
+function Home() {
+  return (
+    <>
+        {/* <CardFrontView /> */}
+        {/* <CardBackView /> */}
+        <Form />
+    </>
+  )
+}
+
+export default Home;

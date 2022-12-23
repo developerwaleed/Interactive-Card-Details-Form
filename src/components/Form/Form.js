f4f6a0680aad129cfa3b982f41bc7782b31de80b
Waleed Amjad <waleedamjad99@hotmail.com>
2022-12-24 01:00:15 +0500

    Added State to form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,24 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Form.css'
 
 function Form() {
+  let [data, setData] = useState({});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('data=', data);
+  }
+
   return (
     <form>
         <label>
             <div>CARDHOLDER NAME</div>
-            <input type="text" name="name" placeHolder="e.g. Jane Appleaseed" className='input-fields'/>
+            <input type="text" name="CardHolderName" placeHolder="e.g. Jane Appleaseed" onChange={(ev)=> setData({...data, name: ev.target.value})} className='input-fields'/>
         </label>
         <label>
             <div>CARD NUMBER</div>
-            <input type="text" name="name" placeHolder="e.g 1234 5678 9123 0000" className='input-fields'/>
+            <input type="text" name="cardNumber" placeHolder="e.g 1234 5678 9123 0000" onChange={(ev)=> setData({...data, cardNumber: ev.target.value})} className='input-fields'/>
         </label>
         <label className='date-cvc-label'>
             <div className='date-cvc-name'>EXP. DATE(MM/YY) <span>CVC</span></div>
-            <input type="number" placeholder='MM' name="name" className='input-fields month-field'/>
-            <input type="number" placeholder='YY' name="name" className='input-fields year-field'/>
-            <input type="number" placeholder='e.g. 123' name="name" className='input-fields CVC-field'/>
+            <input type="number" placeholder='MM' name="month" onChange={(ev)=> setData({...data, month: ev.target.value})} className='input-fields month-field'/>
+            <input type="number" placeholder='YY' name="year" onChange={(ev)=> setData({...data, year: ev.target.value})} className='input-fields year-field'/>
+            <input type="number" placeholder='e.g. 123' name="cvc" onChange={(ev)=> setData({...data, cvc: ev.target.value})} className='input-fields CVC-field'/>
         </label>
-        <button className='btn-primary'>
+        <button onClick={handleSubmit} className='btn-primary'>
           Confirm
         </button>
     </form>

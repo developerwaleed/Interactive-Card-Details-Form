c6fc3010a71a6b8ba70b32f7b7d776ff87262517
Waleed Amjad <waleedamjad99@hotmail.com>
2022-12-25 04:00:47 +0500

    Added navigation

--- src/components/CompleteState/CompleteState.js
+++ src/components/CompleteState/CompleteState.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import image from '../../images/icon-complete.svg';
 import "./CompleteState.css"
 
 function CompleteState() {
+  const navigate = useNavigate();
+
   return (
     <section className='complete-state'>
     <img src={image} height="70px" width="70px" className='tick-icon'/>
     <h3 className='thankyou-text'>THANK YOU!</h3>
     <p className='tagline'>We've added your card details</p>
-    <button  className='btn-primary btn-complete'>
+    <button  onClick={ () => {navigate('/');}} className='btn-primary btn-complete'>
         Confirm
     </button>
     </section>
